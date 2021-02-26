95c3bd5481f334eba59d1e93164ee0900bb8dafe
David Currie <davidalexandercurrie@gmail.com>
2021-02-26 15:53:34 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Sketch from 'react-p5';
 
 function App() {
+  let x, y;
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    x = p5.windowWidth / 2;
+    y = p5.windowHeight / 2;
+  };
+
+  const draw = p5 => {
+    p5.clear();
+    p5.ellipse(x, y, 70, 70);
+    x += p5.random(-1, 1);
+    y += p5.random(-1, 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="iframe-container">
+        <iframe
+          title="stream"
+          className="iframe"
+          src="https://www.youtube.com/embed/8gkxC22E-sY?controls=0&autoplay=1&mute=1"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      </div>
+      <div className="p5-container">
+        <Sketch setup={setup} draw={draw} />;
+      </div>
+    </>
   );
 }
 
